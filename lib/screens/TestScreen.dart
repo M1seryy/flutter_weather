8e24cf96244e3c885fa41f854b46697d5c41cf2a
M1seryy <62029606+M1seryy@users.noreply.github.com>
2024-10-06 20:28:10 +0300

    map test with widgets

--- lib/screens/TestScreen.dart
+++ lib/screens/TestScreen.dart
@@ -3,77 +3,48 @@ import 'package:flutter/material.dart';
 class ListOutPut extends StatelessWidget {
   ListOutPut({super.key});
 
-  List<MenuItem> settingsItems = [
-    MenuItem(title: "Settings", icon: Icons.settings),
-    MenuItem(title: "Calls", icon: Icons.call_sharp),
-    MenuItem(title: "Language", icon: Icons.language),
-  ];
-
   @override
   Widget build(BuildContext context) {
+    List<TwoWords> TextFields = [
+      TwoWords(text1: "1", text2: "2"),
+      TwoWords(text1: "3", text2: "4"),
+      TwoWords(text1: '5', text2: '6'),
+      TwoWords(text1: '7', text2: '8'),
+    ];
+
+    final textFieldArr = TextFields.map((element) =>
+        TextWidgetFunc(text: element.text1, text2: element.text2)).toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("List Setting"),
-        backgroundColor: Colors.blueGrey,
-      ),
-      body: Container(
-        decoration: BoxDecoration(color: Colors.blueGrey),
-        width: double.infinity,
-        child: SettingsMenu(settingsItems: settingsItems),
-
-        // Container(
-        //   width: 100,
-        //   height: 100,
-        //   decoration: const BoxDecoration(color: Colors.black),
-        // ),
-      ),
-    );
-  }
-}
-
-class SettingsMenu extends StatelessWidget {
-  const SettingsMenu({
-    super.key,
-    required this.settingsItems,
-  });
-
-  final List<MenuItem> settingsItems;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-        children: settingsItems
-            .map((element) => MenuItemWidget(data: element))
-            .toList());
-  }
-}
-
-class MenuItem {
-  final String title;
-  final IconData icon;
-
-  MenuItem({required this.title, required this.icon});
-}
-
-class MenuItemWidget extends StatelessWidget {
-  final MenuItem data;
-  const MenuItemWidget({super.key, required this.data});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      decoration: BoxDecoration(color: Colors.white),
-      child: Row(
-        children: [
-          Icon(data.icon),
-          SizedBox(
-            width: 15,
+        appBar: AppBar(
+          title: const Text("List Setting"),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            children: textFieldArr,
           ),
-          Expanded(child: Text(data.title)),
-          Icon(Icons.chevron_right)
-        ],
-      ),
+        ));
+  }
+}
+
+class TwoWords {
+  final String text1;
+  final String text2;
+
+  TwoWords({required this.text1, required this.text2});
+}
+
+class TextWidgetFunc extends StatelessWidget {
+  final String text;
+  final String text2;
+  const TextWidgetFunc({super.key, required this.text, required this.text2});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [Text(text), Spacer(), Text(text2)],
     );
   }
 }
